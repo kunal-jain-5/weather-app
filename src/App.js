1a62f9e247d97edd0b686c5eb52f2fa8e0b70c1d
Kunal Jain <jkunal526@gmail.com>
2021-07-15 14:11:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const baseUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+  const unitAndKey = "&units=metric&appid=06c13f2c71d0f89c96f1e4719921ebd4";
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState("");
+  const [temp, setTemp] = useState("");
+  const submitHandle = (e) => {
+    e.preventDefault();
+    axios.get(baseUrl + city + unitAndKey)
+      .then((res) => {
+      setWeather(res.data.weather.map((weather) => weather.description));
+      setTemp(res.data.main.temp);
+    });
+  };
   return (
     <div className="App">
-      
+      <form onSubmit={submitHandle}>
+        <input
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          placeholder="enter city"
+          type="text"
+        />
+      </form>
+      <div>description: {weather}</div>
+      <div>temp: {temp} °C</div>
     </div>
   );
 }
